bf55a034f592a03d01c5aee14dc4659d0ccfeefa
ahmedebid <ahmed-ebid@outlook.com>
2022-08-03 20:32:46 +0200

    Add the main functionality of the app
    This update adds the ability to add notes, save and update their title and body in state. Also, it applies an abstract theme to the app.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import Note from "./components/Note";
+import {useState} from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote() {
+    setNotes(prevNotes => [
+      ...prevNotes,
+      {
+        id: nanoid(),
+        noteTitle: "",
+        noteBody: ""
+      }
+    ]);
+  }
+
+  function updateNote(e, id) {
+    const {name, value} = e.target;
+    setNotes(prevNotes => prevNotes.map(note => {
+      return note.id === id
+          ? { ...note, [name]: value }
+          : note
+    }));
+  }
+
+  const notesList = notes.map(note => 
+    <Note 
+      key={note.id} 
+      id={note.id} 
+      title={note.noteTitle} 
+      body={note.noteBody}
+      updateNote={updateNote} 
+  />);
+
+  console.log(notes);
+
   return (
     <main>
       <h1>Sticky</h1>
+      <input 
+        type="text" 
+        placeholder="Type here to search..."
+        // value={name}
+        // onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={addNote}>ADD NOTE</button>
       <div className="notes-list">
-        <Note />
-        <Note />
-        <Note />
-        <Note />
+        {notesList}
       </div>
     </main>
   );
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,24 @@
-export default function Note() {
+import deleteIcon from "../images/delete-icon.svg";
+
+export default function Note(props) {
     return(
         <div className="note">
-            <p>Note</p>
+            <img src={deleteIcon} alt="delete name tag" className="delete-note" />
+            <input 
+                className="note-title"
+                type="text" 
+                placeholder="Note title"
+                name="noteTitle"
+                value={props.title}
+                onChange={(e) => props.updateNote(e, props.id)}
+            />
+            <textarea 
+                className="note-body"
+                placeholder="Write the note here..."
+                name="noteBody"
+                value={props.body}
+                onChange={(e) => props.updateNote(e, props.id)}
+            />
         </div>
     );
 }
